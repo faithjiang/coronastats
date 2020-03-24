6718abf718370c32f40da04c885a825c5395d12d
Faith Jiang <faithjiang@Faiths-MBP.hitronhub.home>
2020-03-23 21:38:49 -0700

    added Select Countries on the side

---
+++ client/build/precache-manifest.85b67c160c68d852b1aa91ee515ec137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ed7a4d9a820df0c82c989fc8f81f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98ad739414fcdbc8b952",
+    "url": "/static/css/main.e31ef8aa.chunk.css"
+  },
+  {
+    "revision": "1c1ece907361fef3e867",
+    "url": "/static/js/2.056cc8aa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.056cc8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ad739414fcdbc8b952",
+    "url": "/static/js/main.a5ff0909.chunk.js"
+  },
+  {
+    "revision": "e91bb28b81e0b7a50ff8",
+    "url": "/static/js/runtime-main.11799508.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,176 @@
 import React, {Component} from 'react';
 import Chart from "react-google-charts";
+import Form from 'react-bootstrap/Form';
 import './App.css';
+const {countries} = require('./countries');
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      countries: countries,
+      chart: null
+    };
   }
 
   componentDidMount = () => {
     this.getChart();
   }
 
+  createCountryButton = country => {
+    let checkBox = this.createCheckBox(country);
+    return (
+      <div className='country-btn'>
+        {checkBox}
+        <button type='button' onClick={() => {this.toggleProvinces(country)}}><i className="fas fa-angle-down"></i></button>
+      </div>
+    )
+  }
+
+  toggleProvinces = country => {
+    let tmpCountry = this.state.countries;
+    tmpCountry[country]['flag'] = !(this.state.countries[country]['flag']);
+
+    this.setState({countries : tmpCountry});
+  }
+
+  createSelectTable = () => {
+    let _countries = this.state.countries;
+    let selectList = [];
+    for (let country in _countries) {
+      if ('provinces' in _countries[country]) {
+        selectList.push(this.createAccordion(country))
+      } else {
+        let checkBox = this.createCheckBox(country);
+        selectList.push(checkBox);
+      }
+    }
+    return <div className='SelectBox'>{selectList}</div>; 
+  }
+
+  createAccordion = country => {
+    let provincesList = this.getProvincesDiv(country);
+    let countryButton = this.createCountryButton(country);
+    return ( 
+      <div key={country}>
+        {countryButton}
+        <div className='provinces'>{provincesList}</div>
+      </div>
+    )
+  }
+  
+  getProvincesDiv = country => {
+    if (this.state.countries[country]['flag'] === false) { return <div></div> }
+    return this.createProvincesList(country);
+  }
+
+  createProvincesList = country => {
+    let provincesList = [];
+    for (let province in this.state.countries[country]['provinces']) {
+      provincesList.push(this.createCheckBox(country, province));
+    }
+    return provincesList;
+  }
+
+  createCheckBox = (country, province) => {
+    let labelText = province? province: country;
+    let checkValue = province? this.state.countries[country]['provinces'][province] : this.state.countries[country]['checked'];
+    return (
+      <Form.Check 
+        key={labelText} 
+        type='checkbox' 
+        onChange={e => {this.handleChange(e)}}
+        country={country} 
+        province={province} 
+        label={labelText} 
+        checked={checkValue}/>
+    )
+  }
+
+  handleChange = e => {
+    let country = e.target.getAttribute('country');
+    let province = e.target.getAttribute('province');
+    let tmpCountry = this.state.countries;
+    if (e.target.checked) {
+      if (province) {
+        tmpCountry[country] = this.add2Provinces(tmpCountry[country], province);
+      } else {
+        tmpCountry[country] = this.add2Country(tmpCountry[country]);
+      }
+    } else {
+      if (province) {
+        tmpCountry[country] = this.removeFrmProvinces(tmpCountry[country], province);
+      } else {
+        tmpCountry[country] = this.removeFrmCountry(tmpCountry[country]);
+      }
+    }
+    this.setState({countries: tmpCountry});
+    this.getChart();
+  }
+
+  add2Provinces = (countryObj, province) => {
+    let tmpCountry = countryObj;
+    tmpCountry.provinces[province] = true;
+    if (this.allProvincesChecked(tmpCountry.provinces)) { 
+      tmpCountry.checked = true;
+    } 
+    return tmpCountry;
+  }
+
+  allProvincesChecked = provinceList => {
+    for (let checked of Object.values(provinceList)) {
+      if (checked === false) {return false}
+    }
+    return true;
+  }
+
+  add2Country = countryObj => {
+    let tmpCountry = countryObj;
+    tmpCountry.checked = true;
+    if ('provinces' in tmpCountry) {
+      tmpCountry.provinces = this.setAllProvinces(tmpCountry.provinces, true)
+    }
+    return tmpCountry;
+  }
+
+  setAllProvinces = (provincesObj, value) => {
+    let tmpProvinces = provincesObj;
+    for (let prov in tmpProvinces) {
+      tmpProvinces[prov] = value;
+    }
+    return tmpProvinces;
+  }
+
+  removeFrmProvinces = (countryObj, province) => {
+    let tmpCountry = countryObj;
+    tmpCountry.provinces[province] = false;
+    tmpCountry.checked = false;
+    return tmpCountry;
+  }
+
+  removeFrmCountry = countryObj => {
+    let tmpCountry = countryObj;
+    tmpCountry.checked = false;
+    if ('provinces' in tmpCountry) {
+      tmpCountry.provinces = this.setAllProvinces(tmpCountry.provinces, false);
+    }
+    return tmpCountry;
+  }
+
   getChart = () => {
-    fetch('/getData', {
+    let query = this.getQuery();
+    if (query.length < 1 ){return}
+    fetch(`/stats?${query}`, {
       method: 'GET',
       headers: {'Content-Type': 'application/json'},
     })
     .then(res=> res.json())
     .then(resData => {
       this.createLineGraph(resData);
-    }).catch(err => {
-      throw(err);
-    });
-    this.createLineGraph("test");
+    }).catch(err => { 
+      this.setState({chart: err.toString()});
+      });
   }
 
   createLineGraph = resData => {
@@ -42,22 +189,22 @@ class App extends Component {
       title: 'COVID-19 Statistics',
       chartArea: {
         backgroundColor: _bgcolor,
-        width: '50%',
-        height: '70%'
+        width: '80%',
+        height: '80%'
       },
-      width: '700px',
-      height: '600px',
+      width: '100%',
+      height: '100%',
       hAxis: {
         title: 'Day',
         textStyle: _textStyle,
         titleTextStyle: _textStyle,
-        gridlines: {color: '#465164', minSpacing: 20}
+        gridlines: {color: '#465164', minSpacing: 10}
       },
       vAxis: {
         title: 'Number of Cases',
         textStyle: _textStyle,
         titleTextStyle: _textStyle,
-        gridlines: {color: '#465164', minSpacing: 40}
+        gridlines: {color: '#465164', minSpacing: 100}
       },
       trendlines: {
         0: {
@@ -86,13 +233,41 @@ class App extends Component {
     this.setState({chart: _chart});
   }
 
+  getQuery = () => {
+    let query = {country: [], province: []};
+    let _countries = this.state.countries;
+    let out = [];
+    for (let country in _countries) {
+      if (country === 'chart') { continue }
+      if (_countries[country].checked === true) {
+        query.country.push(country);
+      } else {
+        if (!('provinces' in _countries[country])) { continue }
+        for (let province in _countries[country]['provinces']) {
+          if (_countries[country][province] === true) {
+            query.province.push(country+'-'+province);
+          }
+        }
+      }
+    }
+
+    if (query.country.length > 0) {
+      out.push('country=' + query.country.join(','));
+    }
+    if (query.province.length > 0) {
+      out.push('&province=' + query.province.join(','));
+    }
+    return out.join('&');
+  }
 
   render() {
+    let selectBar = this.createSelectTable();
     return (
       <div className="App">
         <h1>COVID-19 Stats</h1>
         <div className='Dashboard'>
           <div className='Card'>{this.state.chart}</div>
+          {selectBar}
         </div>
       </div>
     );
